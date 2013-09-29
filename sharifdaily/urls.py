31aabe7ad30eb5690288622987bd9348b2a52442
mmbrian <mohsen.brian@gmail.com>
2013-09-29 17:53:45 +0000

    enabled admin, added basic templates

--- sharifdaily/urls.py
+++ sharifdaily/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sharifdaily.views.home', name='home'),
     # url(r'^sharifdaily/', include('sharifdaily.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )

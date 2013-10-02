8470e5526842f0a66475a71c4cf0cd1c158487ef
mmbrian <mohsen.brian@gmail.com>
2013-10-02 09:46:23 +0000

    created another virtualenv

--- sharifdaily/wsgi.py
+++ sharifdaily/wsgi.py
@@ -13,10 +13,12 @@ middleware here, or combine a Django application with an application of another
 framework.
 """
 import os, sys
-
+import site
 # activate virtualenv
-activate_this = os.path.expanduser('/root/sharifdaily-venv/bin/activate_this.py')
+# activate_this = os.path.expanduser('/root/sharifdaily-venv/bin/activate_this.py')
+activate_this = os.path.expanduser('/home/dsuser/sharifdaily2-venv/bin/activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
+site.addsitedir('/home/dsuser/sharifdaily2-venv/lib/python2.7/site-packages')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use

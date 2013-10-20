daad305d7f39130eee2a14d4ff9452210ea15705
Mohsen Mansouryar <mohsen.brian@gmail.com>
2013-10-20 17:10:48 +0330

    enabled easier moderation of posts & comments from admin panel

--- sharifdaily/newspaper/admin.py
+++ sharifdaily/newspaper/admin.py
@@ -4,11 +4,32 @@ from .models import Report, Article, ArticleComment, ReportComment, Archive
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('view_count',)
     exclude = ('tag',)
+    actions = ['make_public']
+
+    def make_public(self, request, queryset):
+        rows_updated = queryset.update(published=True)
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = "%s posts were" % rows_updated
+        self.message_user(request, "%s successfully marked as published." % message_bit)
+    make_public.short_description = "Make selected items as published"
+
 
 class CommentAdmin(admin.ModelAdmin):
     # readonly_fields = ('author', 'created')
     readonly_fields = ('created',)
     exclude = ('tag',)
+    actions = ['make_public']
+
+    def make_public(self, request, queryset):
+        rows_updated = queryset.update(is_public=True)
+        if rows_updated == 1:
+            message_bit = "1 comment was"
+        else:
+            message_bit = "%s comments were" % rows_updated
+        self.message_user(request, "%s successfully marked as published." % message_bit)
+    make_public.short_description = "Make selected comments as public"
 
 class ArchiveAdmin(admin.ModelAdmin):
     # exclude = ('tag',)

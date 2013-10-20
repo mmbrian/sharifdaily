0051218608332a11dadea327e17af3cfd88696e5
mmbrian <mohsen.brian@gmail.com>
2013-10-20 06:47:37 +0000

    added profile change picture & change password capability, plus loading user profile information

--- sharifdaily/accounts/urls.py
+++ sharifdaily/accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('sharifdaily.accounts.views',
 	url(r'^activate/(?P<username>.*)/(?P<confirmation_code>.*)', 'activate'),
 	url(r'^register/get_registeration_key', 'register_key'),
 	url(r'^register$', 'register'),
+	url(r'^change_password$', 'change_password'),
+	url(r'^change_picture$', 'change_picture'),
 )
--- sharifdaily/accounts/views.py
+++ sharifdaily/accounts/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.core.mail import send_mail, BadHeaderError
+from django.core.serializers.json import DjangoJSONEncoder
 
 from random import choice
 from string import digits, ascii_lowercase, ascii_uppercase
@@ -11,6 +12,11 @@ from string import digits, ascii_lowercase, ascii_uppercase
 from .utils import user_present, diff
 from .models import Profile
 from sharifdaily.settings import SERVER_ADDRESS
+
+try:
+    import json
+except ImportError:
+   from django.utils import simplejson as json
 
 SECRET_KEY = 'sharif0@4-64m+wcv*#l2-ula5qq5gd@-bn-#^8&8&axfz3zrp48!x7=daily'
 # rot13(base64(SECRET_KEY)) > use this for registration
@@ -23,11 +29,72 @@ def login(request, uname, pwd):
 	user = authenticate(username=uname, password=pwd)
 	if user is not None:
 		if user.is_active:
-			return HttpResponse(unicode(str(user.id) + '|' + user.get_full_name()))
+			user_id        = user.id
+			user_full_name = user.get_full_name()
+			user_major     = ''
+			user_avatar    = ''
+			try:
+				user_major = user.profile.major
+				user_avatar= user.profile.avatar.url # sth like /uploads/avatars/...
+			except ValueError:
+				user_avatar= ''
+			except Profile.DoesNotExist:
+				user_major = ''
+				user_avatar= ''
+
+			keys = ["id", "full_name", "major", "avatar_url"]
+			values = [user_id, user_full_name, user_major, user_avatar]
+			return HttpResponse(json.dumps(dict(zip(keys, values)), cls=DjangoJSONEncoder))
 		else:
 			return HttpResponse("deactive")
 	else:
 		return HttpResponse("invalid")
+
+@csrf_exempt
+def change_password(request):
+	if request.method == 'POST':
+		key = request.POST['key']
+		if diff(key, REAL_SECRET_KEY) < 3:
+			uname = request.POST['username']
+			pwd   = request.POST['old_password']
+			user  = authenticate(username=uname, password=pwd)
+			if user is not None:
+				new_pwd = request.POST['new_password']
+				user.set_password(new_pwd)
+				user.save()
+				return HttpResponse('changed')
+			else:
+				return HttpResponse('invalid user')
+		else:
+			return HttpResponse('invalid key')
+	else:
+		return HttpResponse('invalid request')
+
+@csrf_exempt
+def change_picture(request):
+	if request.method == 'POST':
+		key = request.POST['key']
+		if diff(key, REAL_SECRET_KEY) < 3:
+			user_id = request.POST['user_id']
+			try:
+				user = User.objects.get(id=int(user_id))
+				request_type = request.POST.get('type', '')
+				image = request.FILES.get('image', None)
+				if (request_type == 'set'):
+					user.profile.avatar = image
+				else:
+					user.profile.avatar.delete()
+				user.save()
+				return HttpResponse("changed")
+			except User.DoesNotExist:
+				return HttpResponse("invalid user")
+			except Exception as error:
+				# return HttpResponse(str(error))
+				return HttpResponse('invalid operation')
+		else:
+			return HttpResponse('invalid key')
+	else:
+		return HttpResponse('invalid request')
 
 @csrf_exempt
 def register(request):
@@ -68,6 +135,7 @@ def activate(request, username, confirmation_code):
 		return render_to_response('accounts/signup_complete.html', locals())
 	except User.DoesNotExist:
 		return HttpResponse('invalid')
+
 
 def send_confirmation_email(username, to_email, confirmation_code):
 	message = 'Greetings!\n\nVisit %saccounts/activate/%s/%s in order to activate your account.\n\nThanks' % (SERVER_ADDRESS, username, confirmation_code)

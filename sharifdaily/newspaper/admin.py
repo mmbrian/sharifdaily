ec04d069066f6eee9d74173b67ed62a4930768fe
Mohsen Mansouryar <mohsen.brian@gmail.com>
2013-11-26 11:49:51 +0330

    added Ad to admin interface

--- sharifdaily/newspaper/admin.py
+++ sharifdaily/newspaper/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Report, Article, ArticleComment, ReportComment, Archive
+from .models import Report, Article, ArticleComment, ReportComment, Archive, Ad
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('view_count',)
@@ -35,8 +35,12 @@ class ArchiveAdmin(admin.ModelAdmin):
     # exclude = ('tag',)
     pass
 
+class AdAdmin(admin.ModelAdmin):
+    exclude = ('tag',)
+
 admin.site.register(Report, PostAdmin)
 admin.site.register(Article, PostAdmin)
 admin.site.register(ArticleComment, CommentAdmin)
 admin.site.register(ReportComment, CommentAdmin)
 admin.site.register(Archive, ArchiveAdmin)
+admin.site.register(Ad, AdAdmin)
